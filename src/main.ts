7a162f590467868aa1b5a28d72a1b524ce6cf189
NivNaim <nivnaimwork@gmail.com>
2024-07-18 20:48:19 +0300

    feat: added hot reload for the application

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+declare const module: any;
 
 async function bootstrap() {
+  const logger = new Logger();
+  const port = process.env.PORT;
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  logger.log(`Application listening on port ${port || 3000}`);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();

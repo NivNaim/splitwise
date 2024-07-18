b4a298c6a6fc8454870944b625f9dae3ccca06ed
NivNaim <nivnaimwork@gmail.com>
2024-07-19 00:02:26 +0300

    feat: added configuration to store env variables for db

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,21 +3,30 @@ import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GroupModule } from './group/group.module';
 import { ExpenseModule } from './expense/expense.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     AuthModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      autoLoadEntities: true,
-      synchronize: true,
-      entities: [__dirname + '/../**/*.entity.js'],
-      username: 'postgres',
-      password: 'postgres',
-      database: 'splitwise',
-    }),
     GroupModule,
     ExpenseModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        autoLoadEntities: true,
+        synchronize: true,
+        entities: [__dirname + '/../**/*.entity.js'],
+        host: configService.get('DB_HOST'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+      }),
+    }),
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+    }),
   ],
 })
 export class AppModule {}

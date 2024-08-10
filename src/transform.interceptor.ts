e0186de404726a72fce7dabde60a7d773dc4d7c0
NivNaim <nivnaimwork@gmail.com>
2024-08-11 02:08:40 +0300

    feat: added return type for intercept method

--- src/transform.interceptor.ts
+++ src/transform.interceptor.ts
@@ -5,15 +5,11 @@ import {
   CallHandler,
 } from '@nestjs/common';
 import { instanceToPlain } from 'class-transformer';
-import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable()
 export class TransformInterceptor implements NestInterceptor {
-  intercept(
-    context: ExecutionContext,
-    next: CallHandler<any>,
-  ): Observable<Record<string, any>> {
+  intercept(context: ExecutionContext, next: CallHandler<any>) {
     return next.handle().pipe(map((data) => instanceToPlain(data)));
   }
 }
